8e3ae8e15e95fae6f0abed43af1cc6449142243e
Iarlen <iarlensr@hotmail.com>
2024-05-12 19:37:09 -0300

    feat: create routes for entering and leaving an event.

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,7 @@ Route::delete('/events/{id}', [EventController::class,'destroy'])->middleware('a
 Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
 Route::put('/events/edit/{id}', [EventController::class,'update'])->middleware('auth');
 
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
+
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
