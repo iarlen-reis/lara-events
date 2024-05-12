7899857f91bc44cc42a6823640b18a223460ca3d
Iarlen <iarlensr@hotmail.com>
2024-05-12 19:36:50 -0300

    feat: create methods for entering and leaving an event.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -49,17 +49,28 @@ class EventController extends Controller
     }
 
     public function dashboard() {
-        $user = auth()->user();
+        $user = User::findOrFail(auth()->user()->id);
 
         $events = $user->events;
 
+        $userEvents = $user->eventsAsParticipant()->get();
+
         return view('events.dashboard', [
             'events'=> $events,
+            'userEvents' => $userEvents,
         ]);
     }
 
     public function destroy($id) {
-        Event::findOrFail($id)->delete();
+        $user = auth()->user();
+        $event = Event::findOrFail($id);
+
+        if ($event->user_id != $user->id) {
+            return redirect("/");
+        }
+
+        $event->users()->detach();
+        $event->delete();
 
         return redirect('/dashboard')
             ->with('message', 'Evento foi excluído com sucesso!');
@@ -77,8 +88,36 @@ class EventController extends Controller
     public function edit($id) {
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+
+        if ($event->user_id != $user->id) {
+            return redirect("/");
+        }
+
         return view("events.edit", [
             "event"=> $event,
         ]);
+    }
+
+    public function joinEvent($id) {
+        User::findOrFail(auth()
+            ->user()
+            ->id)
+            ->eventsAsParticipant()
+            ->attach($id);
+
+        return redirect("/events/".$id)
+            ->with("message","Sua presença foi confirmada!");
+    }
+
+    public function leaveEvent($id) {
+        User::findOrFail(auth()
+            ->user()
+            ->id)
+            ->eventsAsParticipant()
+            ->detach($id);
+
+        return redirect("/events/".$id)
+            ->with("message","Sua presença foi removida!");
     }
 }
